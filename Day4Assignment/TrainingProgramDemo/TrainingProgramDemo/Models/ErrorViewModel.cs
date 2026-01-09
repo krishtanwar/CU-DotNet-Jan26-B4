67186b0570b2268e47f0dc293a3effda4f876048
krish <krishtanwar20092003@gmail.com>
2026-01-09 15:23:31 +0530

    krish

---
+++ Day4Assignment/TrainingProgramDemo/TrainingProgramDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TrainingProgramDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
