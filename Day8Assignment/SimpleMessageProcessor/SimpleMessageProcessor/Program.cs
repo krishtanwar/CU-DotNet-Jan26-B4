bc1d47227f7b59fab0a143ec3458d778bf6c0c21
krish <krishtanwar20092003@gmail.com>
2026-01-14 14:33:31 +0530

    Day8Assignment

---
+++ Day8Assignment/SimpleMessageProcessor/SimpleMessageProcessor/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace SimpleMessageProcessor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            string status = string.Empty;
+            Console.WriteLine("Enter input in the form abc|xyz");
+            input = Console.ReadLine();
+            string[] parts = input.Split('|', StringSplitOptions.RemoveEmptyEntries |  StringSplitOptions.TrimEntries);
+            string name = parts[0];
+            string message = parts[1].ToLower();
+
+            if (!message.Contains("successful"))
+            {
+                status = "LOGIN FAILED";
+            }
+            else if(message.Equals("login successful"))
+            {
+                status = "LOGIN SUCCESS";
+            }
+            else
+            {
+                status = "LOGIN SUCCESS (CUSTOM MESSAGE)";
+            }
+
+            Console.WriteLine($"User  :{ name}");
+            Console.WriteLine($"Message  : { message}");
+            Console.WriteLine($"Status  :{status}");
+
+            Console.WriteLine(" ");
+
+        }
+    }
+}
