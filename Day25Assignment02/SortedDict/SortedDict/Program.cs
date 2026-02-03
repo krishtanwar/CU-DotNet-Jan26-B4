583504abbcf45d3c99bd4419afd2707748b04bcc
krish <krishtanwar20092003@gmail.com>
2026-02-03 14:44:03 +0530

    Done

---
+++ Day25Assignment02/SortedDict/SortedDict/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SortedDict
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<double, string> leaderboard = new SortedDictionary<double, string>();
+            leaderboard.Add(55.42, "SwiftRacer");
+            leaderboard.Add(52.10, "SpeedDemon");
+            leaderboard.Add(58.91, "SteadyEddie");
+            leaderboard.Add(51.05, "TurboTom");
+
+            foreach(var item in leaderboard)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-------------------------");
+            Console.WriteLine($"The Topper of the Leaderboard is: {leaderboard.First()}");
+
+            leaderboard.Remove(58.91);
+
+            leaderboard.Add(54, "SteadyEddie");
+            Console.WriteLine("-------------------------");
+
+            Console.WriteLine("After Updation: ");
+
+            foreach (var item in leaderboard)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
