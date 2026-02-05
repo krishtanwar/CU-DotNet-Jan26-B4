c60afa22618b94adb8f2cb06dffc296f4d59df2d
krish <krishtanwar20092003@gmail.com>
2026-02-05 09:56:31 +0530

    Done

---
+++ Day25Assignment01/ReadKey/ReadKey/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace ReadKey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Pin: ");
+            int i = 0;
+            ConsoleKeyInfo[] c = new ConsoleKeyInfo[4];
+            char[] ch=new char[4];
+            
+
+            while (true)
+            {
+                ConsoleKeyInfo Key = Console.ReadKey(true);
+                if (char.IsLetterOrDigit(Key.KeyChar)&&i<4)
+                {
+                    ch[i] = Key.KeyChar;
+                    Console.Write("*");
+                    i++;
+                }
+                else if (Key.Key == ConsoleKey.Backspace && i > 0)
+                {
+                    i--;
+                    Console.Write("\b \b");
+                }
+                else if(Key.Key == ConsoleKey.Enter)
+                {
+                    if (i == 4) break;
+
+                    else Console.Beep();
+                }
+                else
+                {
+                    Console.Beep();
+                }
+            }
+            
+                Console.WriteLine();
+            Console.WriteLine("\nYour PIN is:");
+            for (int j=0; j < ch.Length; j++)
+            {
+                Console.Write(ch[j]);
+            }
+        }
+    }
+}
