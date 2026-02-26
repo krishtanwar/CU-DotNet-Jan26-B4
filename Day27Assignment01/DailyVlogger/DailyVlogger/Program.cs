6feb8858049e80a5a72f340b425a955e8cae992f
krish <krishtanwar20092003@gmail.com>
2026-02-26 21:55:02 +0530

    Done

---
+++ Day27Assignment01/DailyVlogger/DailyVlogger/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace DailyVlogger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string filePath = "journal.txt";
+
+            Console.WriteLine("Enter your Daily Reflection:");
+            string reflection = Console.ReadLine();
+
+            using (StreamWriter writer = new StreamWriter(filePath, true))
+            {
+                writer.WriteLine("Date: " + DateTime.Now);
+                writer.WriteLine(reflection);
+                writer.WriteLine("----------------------------");
+            }
+
+            Console.WriteLine("Your reflection has been saved.");
+            Console.ReadLine();
+        }
+    }
+}
