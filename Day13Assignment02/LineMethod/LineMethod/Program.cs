dd3b570134314760fb159bb4838ca2d9918bc2a8
krish <krishtanwar20092003@gmail.com>
2026-02-26 21:41:53 +0530

    Done

---
+++ Day13Assignment02/LineMethod/LineMethod/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace LineMethod
+{
+    internal class Program
+    {
+        public static void DisplayLine()
+        {
+            for (int i = 0; i < 40; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine();
+        }
+
+        public static void DisplayLine(char ch)
+        {
+            for (int i = 0; i < 40; i++)
+            {
+                Console.Write(ch);
+            }
+            Console.WriteLine();
+        }
+
+      
+        public static void DisplayLine(char ch, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(ch);
+            }
+            Console.WriteLine();
+        }
+
+        
+        static void Main(string[] args)
+        {
+            DisplayLine();          
+            DisplayLine('+');       
+            DisplayLine('$', 60);
+        }
+    }
+}
