8282bd5b3a3bfb5a5adccdbfada9ddb832ac14be
krish <krishtanwar20092003@gmail.com>
2026-02-26 21:48:12 +0530

    Done

---
+++ Day16Assignment01/StaticConfig/StaticConfig/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace StaticConfig
+{
+    internal class Program
+    {
+
+        public static class ApplicationConfig
+        {
+            // Static Properties
+            public static string ApplicationName { get; set; }
+            public static string Environment { get; set; }
+            public static int AccessCount { get; set; }
+            public static bool IsInitialized { get; set; }
+
+            // Static Constructor
+            static ApplicationConfig()
+            {
+                ApplicationName = "MyApp";
+                Environment = "Development";
+                AccessCount = 0;
+                IsInitialized = false;
+
+                Console.WriteLine("Static constructor executed");
+            }
+
+            // Static Method: Initialize
+            public static void Initialize(string appName, string environment)
+            {
+                ApplicationName = appName;
+                Environment = environment;
+                IsInitialized = true;
+                AccessCount++;
+            }
+
+            // Static Method: GetConfigurationSummary
+            public static string GetConfigurationSummary()
+            {
+                AccessCount++;
+
+                return $"Application Name : {ApplicationName}\n" +
+                       $"Environment      : {Environment}\n" +
+                       $"Access Count     : {AccessCount}\n" +
+                       $"Is Initialized   : {IsInitialized}";
+            }
+
+            // Static Method: ResetConfiguration
+            public static void ResetConfiguration()
+            {
+                ApplicationName = "MyApp";
+                Environment = "Development";
+                IsInitialized = false;
+                AccessCount++;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ApplicationConfig.ApplicationName);
+
+            ApplicationConfig.Initialize("GymApp", "Production");
+
+            string summary = ApplicationConfig.GetConfigurationSummary();
+            Console.WriteLine("\nConfiguration Summary:");
+            Console.WriteLine(summary);
+
+            ApplicationConfig.ResetConfiguration();
+
+            string summary2 = ApplicationConfig.GetConfigurationSummary();
+            Console.WriteLine("\nAfter Reset:");
+            Console.WriteLine(summary2);
+
+            Console.ReadLine();
+        }
+    }
+}
+
