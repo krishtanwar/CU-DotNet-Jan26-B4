0cef8ed38722aacac2fe35b18cbd5555f35fc6b0
krish <krishtanwar20092003@gmail.com>
2026-02-26 21:35:59 +0530

    Done

---
+++ Day13Assignment01/GymCharges/GymCharges/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace GymCharges
+{
+    internal class Program
+    {
+        public static double CalculateMembershipAmount(bool treadmill, bool weightLifting, bool zumba)
+        {
+            double total = 1000; // fixed monthly charge
+            int serviceCount = 0;
+
+            if (treadmill)
+            {
+                total += 300;
+                serviceCount++;
+            }
+
+            if (weightLifting)
+            {
+                total += 500;
+                serviceCount++;
+            }
+
+            if (zumba)
+            {
+                total += 250;
+                serviceCount++;
+            }
+
+            
+            if (serviceCount == 0)
+            {
+                total += 200;
+            }
+
+            
+            double gst = total * 0.05;
+            total += gst;
+
+            return total;
+        }
+        static void Main(string[] args)
+        {
+            double amount = CalculateMembershipAmount(true, false, true);
+            Console.WriteLine("Total Amount = Rs. " + amount);
+        }
+    }
+}
