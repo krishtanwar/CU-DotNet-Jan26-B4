55df65d645cec78aef06276a6213bf93b3ed381d
krish <krishtanwar20092003@gmail.com>
2026-02-27 15:06:06 +0530

    Done

---
+++ Day43Assignment01/FinancialPortfolioMngmnt/FinancialPortfolioMngmnt/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FinancialPortfolioMngmnt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
