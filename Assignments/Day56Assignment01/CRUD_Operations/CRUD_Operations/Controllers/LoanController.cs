abc56590505e8847433026bb3b0364b2734160ab
krish <krishtanwar20092003@gmail.com>
2026-03-17 11:29:48 +0530

    Added

---
+++ Assignments/Day56Assignment01/CRUD_Operations/CRUD_Operations/Controllers/LoanController.cs
@@ -0,0 +1,78 @@
+﻿using CRUD_Operations.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_Operations.Controllers
+{
+    public class LoanController : Controller
+    {
+        private static List<Loan> loans = new List<Loan>();
+
+        
+        public IActionResult Index()
+        {
+            return View(loans);
+        }
+
+        
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        public IActionResult Add(Loan loan)
+        {
+            if (ModelState.IsValid)
+            {
+                loan.Id = loans.Count + 1;
+                loans.Add(loan);
+                return RedirectToAction("Index");
+            }
+
+            return View(loan);
+        }
+
+        
+        public IActionResult Edit(int id)
+        {
+            var loan = loans.FirstOrDefault(x => x.Id == id);
+            return View(loan);
+        }
+
+       
+        [HttpPost]
+        public IActionResult Edit(Loan loan)
+        {
+            if (ModelState.IsValid)
+            {
+                var existingLoan = loans.FirstOrDefault(x => x.Id == loan.Id);
+
+                if (existingLoan != null)
+                {
+                    existingLoan.BorrowerName = loan.BorrowerName;
+                    existingLoan.LenderName = loan.LenderName;
+                    existingLoan.Amount = loan.Amount;
+                    existingLoan.IsSettled = loan.IsSettled;
+                }
+
+                return RedirectToAction("Index");
+            }
+
+            return View(loan);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var loan = loans.FirstOrDefault(x => x.Id == id);
+
+            if (loan != null)
+                loans.Remove(loan);
+
+            return RedirectToAction("Index");
+        }
+        
+    }
+    
+}
