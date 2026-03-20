a998320ad87608d27c906f9e591e23acba323537
krish <krishtanwar20092003@gmail.com>
2026-03-20 12:36:29 +0530

    Added

---
+++ Assignments/Day62Assignment01/LoanManagementSystem2/LoanManagementSystem2/Data/LoanManagementSystem2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LoanManagementSystem2.Models;
+
+namespace LoanManagementSystem2.Data
+{
+    public class LoanManagementSystem2Context : DbContext
+    {
+        public LoanManagementSystem2Context (DbContextOptions<LoanManagementSystem2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LoanManagementSystem2.Models.Loan> Loan { get; set; } = default!;
+    }
+}
---
+++ Assignments/Day62Assignment01/LoanManagementSystem2/LoanManagementSystem2/Models/Loan.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LoanManagementSystem2.Models
+{
+    public class Loan
+    {
+        public int Id { get; set; }
+        public string BorrowerName { get; set; }
+        [Precision(18,4)]
+        public decimal Amount  { get; set; }
+        public int LoanTermMonths { get; set; }
+        public bool IsApproved { get; set; }
+    }
+}
