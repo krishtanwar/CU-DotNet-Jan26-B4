855674296040c958d144a863ed86d4bff2a253e0
krish <krishtanwar20092003@gmail.com>
2026-03-31 14:40:57 +0530

    Added

---
+++ Assessments/Week-01-Assessment/GreetingLibrary/GreetingLibrary/GreetingHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace GreetingLibrary
+{
+    public class GreetingHelper
+    {
+        public static string GetGreeting(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                return "Hello, Guest!";
+            else
+                return "Hello, " + name + "!";
+        }
+    }
+}
